47e2809cbe1d4e82f4b61dd9bd7c562536051315
snacurzz <snacurzz@gmail.com>
2025-05-13 12:49:20 +0530

    transitionLink.js

--- transitionLink.js
+++
@@ -1,9 +0,0 @@
-document.querySelectorAll('.transition-link').forEach(link => {
-  link.addEventListener('click', function(e) {
-    e.preventDefault();
-    document.body.classList.add('fade-out');
-    setTimeout(() => {
-      window.location.href = this.href;
-    }, 500);
-  });
-});
